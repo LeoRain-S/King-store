6ac110f6229f6643bf9cd4ea31868df04edf1c9b
LeoRain-S <dunyueshi@gamil.com>
2024-07-23 23:47:49 -0400

    ...

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 
 import { 
   createAuthUserWithEmailAndPassowrd, 
@@ -24,15 +24,15 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
+  // const { setCurrentUser } = useContext(UserContext);
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   }
 
   const signInWithGoogle = async () => {
-    const {user} = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
+
   }
 
   const handleSubmit = async (event) => {
@@ -40,7 +40,7 @@ const SignInForm = () => {
 
     try {
       const response = await signInAuthUserWithEmailAndPassowrd(email, password);
-      setCurrentUser(response);
+      // setCurrentUser(response);
       resetFormFields();
     } catch(error) {
       switch(error.code) {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,13 +10,10 @@ import { UserContext } from '../../contexts/user.context';
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
   // console.log(currentUser);
 
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  }
+
   return (
     <Fragment>
       <div className='navigation'>
@@ -29,7 +26,7 @@ const Navigation = () => {
           </Link>
           {
             currentUser ? (
-              <span className='nav-link' onClick={signOutHandler}>
+              <span className='nav-link' onClick={signOutUser}>
                 SIGN OUT
               </span>
             ) : (
